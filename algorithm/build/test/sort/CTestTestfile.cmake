c31e1c2a6f2dcc117497552c250473445a2aabe9
Josh Chen <bochfchen.code@gmail.com>
2023-07-02 11:31:57 -0400

    remove build

--- algorithm/build/test/sort/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/josh/proj/homework/cs32/algorithm/test/sort
-# Build directory: /home/josh/proj/homework/cs32/algorithm/build/test/sort
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_sort]=] "/home/josh/proj/homework/cs32/algorithm/build/test/sort/test_sort")
-set_tests_properties([=[test_sort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/josh/proj/homework/cs32/algorithm/test/sort/CMakeLists.txt;6;add_test;/home/josh/proj/homework/cs32/algorithm/test/sort/CMakeLists.txt;0;")
